48be661ae3455b3b87c8805fbf9c2704d1a62694
ben-du-pont <benjamin.dupont@yahoo.fr>
2023-05-05 11:31:00 +0200

    reset

--- task3/template_solution.py
+++ task3/template_solution.py
@@ -11,9 +11,6 @@ import torchvision.datasets as datasets
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torchvision.models import resnet50, ResNet50_Weights
-from efficientnet_pytorch import EfficientNet
-
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -23,11 +20,7 @@ def generate_embeddings():
     the embeddings.
     """
     # TODO: define a transform to pre-process the images
-    train_transforms = transforms.Compose([
-        transforms.Resize((400, 200)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-    ])
+    train_transforms = transforms.Compose([transforms.ToTensor()])
 
     train_dataset = datasets.ImageFolder(root="task3/dataset/", transform=train_transforms)
     # Hint: adjust batch_size and num_workers to your PC configuration, so that you don't 
@@ -39,33 +32,14 @@ def generate_embeddings():
 
     # TODO: define a model for extraction of the embeddings (Hint: load a pretrained model,
     #  more info here: https://pytorch.org/vision/stable/models.html)
-    
     model = nn.Module()
     embeddings = []
     embedding_size = 1000 # Dummy variable, replace with the actual embedding size once you 
     # pick your model
-    model = EfficientNet.from_pretrained('efficientnet-b0')
-    embedding_size = model._fc.out_features
-
-    # weights = ResNet50_Weights.DEFAULT
-    # model = resnet50(weights=weights)
-
     num_images = len(train_dataset)
     embeddings = np.zeros((num_images, embedding_size))
-
     # TODO: Use the model to extract the embeddings. Hint: remove the last layers of the 
     # model to access the embeddings the model generates. 
-    model = torch.nn.Sequential(*(list(model.children())[:-1]))
-
-    # use the model to extract the embeddings
-    model.to(device)
-    model.eval()
-
-    with torch.no_grad():
-        for i, (images, _) in enumerate(train_loader):
-            images = images.to(device)
-            features = model(images)
-            embeddings[i*64:i*64+len(images)] = features.cpu().numpy()
 
     np.save('task3/dataset/embeddings.npy', embeddings)
 
@@ -90,13 +64,7 @@ def get_data(file, train=True):
                                          transform=None)
     filenames = [s[0].split('/')[-1].replace('.jpg', '') for s in train_dataset.samples]
     embeddings = np.load('task3/dataset/embeddings.npy')
-
     # TODO: Normalize the embeddings across the dataset
-    
-    norm = np.sqrt(np.sum(embeddings**2))
-
-    normalized_embedding = embeddings / norm
-    embeddings = normalized_embedding
 
     file_to_embedding = {}
     for i in range(len(filenames)):
@@ -139,61 +107,26 @@ def create_loader_from_np(X, y = None, train = True, batch_size=64, shuffle=True
 
 # TODO: define a model. Here, the basic structure is defined, but you need to fill in the details
 class Net(nn.Module):
-#     """
-#     The model class, which defines our classifier.
-#     """
-#     def __init__(self):
-#         """
-#         The constructor of the model.
-#         """
-#         super().__init__()
-#         self.fc = nn.Linear(3000, 1)
-
-#     def forward(self, x):
-#         """
-#         The forward pass of the model.
-
-#         input: x: torch.Tensor, the input to the model
-
-#         output: x: torch.Tensor, the output of the model
-#         """
-#         x = self.fc(x)
-#         x = F.relu(x)
-#         return x
-
+    """
+    The model class, which defines our classifier.
+    """
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(num_features=16)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(num_features=32)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(num_features=64)
-        self.relu3 = nn.ReLU(inplace=True)
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
-        self.fc = nn.Linear(in_features=64, out_features=1000)
+        """
+        The constructor of the model.
+        """
+        super().__init__()
+        self.fc = nn.Linear(3000, 1)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
-        x = self.pool2(x)
-        x = self.conv3(x)
-        x = self.bn3(x)
-        x = self.relu3(x)
-        x = self.pool3(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
+        """
+        The forward pass of the model.
+
+        input: x: torch.Tensor, the input to the model
+
+        output: x: torch.Tensor, the output of the model
+        """
         x = self.fc(x)
+        x = F.relu(x)
         return x
 
 def train_model(train_loader):
@@ -214,26 +147,9 @@ def train_model(train_loader):
     # validation split and print it out. This enables you to see how your model is performing 
     # on the validation data before submitting the results on the server. After choosing the 
     # best model, train it on the whole training data.
-
-    criterion = nn.BCEWithLogitsLoss() # Binary cross-entropy loss
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001) # Adam optimizer with learning rate 0.001
-    
-
-    for epoch in range(n_epochs):
-        running_loss = 0.0        
+    for epoch in range(n_epochs):        
         for [X, y] in train_loader:
-            X, y = X.to(device), y.to(device)
-            optimizer.zero_grad() # zero the parameter gradients
-            outputs = model(X) # forward pass
-            loss = criterion(outputs.squeeze(), y.float()) # compute loss
-            loss.backward() # backward pass
-            optimizer.step() # update weights
-            
-            running_loss += loss.item() * X.size(0) # update running loss
-        
-        # print the average training loss for the epoch
-        train_loss = running_loss / len(train_loader.dataset)
-        print('Epoch: {} Training Loss: {:.4f}'.format(epoch+1, train_loss))
+            pass
     return model
 
 def test_model(model, loader):
@@ -264,8 +180,8 @@ def test_model(model, loader):
 
 # Main function. You don't have to change this
 if __name__ == '__main__':
-    TRAIN_TRIPLETS = 'task3/train_triplets.txt'
-    TEST_TRIPLETS = 'task3/test_triplets.txt'
+    TRAIN_TRIPLETS = 'train_triplets.txt'
+    TEST_TRIPLETS = 'test_triplets.txt'
 
     # generate embedding for each image in the dataset
     if(os.path.exists('task3/dataset/embeddings.npy') == False):
